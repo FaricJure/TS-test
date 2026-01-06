e0e85a8d6405f808f9c7acb551f3e38d4c9560e1
FaricJure <jure.faric@gmail.com>
2026-01-06 12:55:15 +0100

    Vite test

--- index.js
+++
@@ -1,8 +0,0 @@
-import { log, chats } from "./chats.js";
-const button = document.getElementById("enable-button");
-button?.addEventListener("click", () => {
-    const supportAI = window.supportAI;
-    supportAI?.enableAutoReply();
-});
-log(chats);
-// @ts-ignore
